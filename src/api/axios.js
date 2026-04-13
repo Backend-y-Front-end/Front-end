801167f2d04f348ed281875b2564cfb194add13a
Fernando Zapata B <1224100205.fzb@gmail>
2026-04-13 07:51:54 -0600

    Front-end

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,19 +1,15 @@
 import axios from "axios";
 
 const clienteAxios = axios.create({
-  // Escribe la URL de tu backend directamente SIN el /api al final
-  baseURL: "https://backend-production-0532.up.railway.app",
+  baseURL: "http://localhost:4000"
 });
 
-clienteAxios.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers["x-auth-token"] = token;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error),
-);
+clienteAxios.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers["x-auth-token"] = token;
+  }
+  return config;
+});
 
 export default clienteAxios;
